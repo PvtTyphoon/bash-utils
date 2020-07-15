7f1aab0f41b9eb98e523867d70f3923dddec727e
Typhoon <hashtyphoon@protonmail.com>
2020-07-16 01:15:13 +0530

    Move up a directory tree with ease

---
+++ functions/up.sh
@@ -0,0 +1,14 @@
+# ~/.bashrc
+up() {
+  local d=""
+  limit=$1
+  for ((i=1 ; i <= limit ; i++))
+    do
+      d=$d/..
+    done
+  d=$(echo $d | sed 's/^\///')
+  if [ -z "$d" ]; then
+    d=..
+  fi
+  cd $d
+}

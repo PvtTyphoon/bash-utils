6523dbfae9bc8ce2042c73ca5078c397da9e3a6e
Typhoon <hashtyphoon@protonmail.com>
2020-07-16 01:31:43 +0530

    Display Network information

--- functions/netinfo.sh
+++ functions/netinfo.sh
@@ -1,6 +1,6 @@
 # ~/.bashrc
 netinfo() {
-        echo "--------------- Network Information ---------------"
+        echo "---------------- Network Information ----------------"
         /sbin/ifconfig | awk /'inet addr/ {print $2}'
         echo ""
         /sbin/ifconfig | awk /'Bcast/ {print $3}'
@@ -8,5 +8,5 @@ netinfo() {
         /sbin/ifconfig | awk /'inet addr/ {print $4}'
 
         /sbin/ifconfig | awk /'HWaddr/ {print $4,$5}'
-        echo "---------------------------------------------------"
+        echo "-----------------------------------------------------"
 }

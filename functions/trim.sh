2ae984fecdc69577c6517e520993c59fa3cf05b4
Typhoon <hashtyphoon@protonmail.com>
2020-07-16 01:17:10 +0530

    Trims whitespace from scripts

---
+++ functions/trim.sh
@@ -0,0 +1,8 @@
+# ~/.bashrc
+trim() {
+local var=$@
+        var="${var#"${var%%[![:space:]]*}"}"  # remove leading whitespace characters
+        var="${var%"${var##*[![:space:]]}"}"  # remove trailing whitespace characters
+        echo -n "$var"
+}
+        

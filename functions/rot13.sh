c56010a2e14f93d366a248cc463ba5c6437629f2
Typhoon <hashtyphoon@protonmail.com>
2020-07-16 01:18:37 +0530

    Converts string  to rot13

---
+++ functions/rot13.sh
@@ -0,0 +1,8 @@
+# ~/.bashrc
+rot13() {
+        if [ $# -eq 0 ]; then
+                tr '[a-m][n-z][A-M][N-Z]' '[n-z][a-m][N-Z][A-M]'
+        else
+                echo $* | tr '[a-m][n-z][A-M][N-Z]' '[n-z][a-m][N-Z][A-M]'
+        fi
+}
